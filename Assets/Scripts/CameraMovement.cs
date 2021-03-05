56797c4bbd5e4145488794299493dd02a32d82c9
AlexandrVinokurov <aavinokurov@outlook.com>
2021-03-05 18:43:34 +0300

    Create: camera movement & game over

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - playerTransform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = playerTransform.position + offset;
+    }
+}
