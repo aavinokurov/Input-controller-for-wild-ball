ae7a1bae8036bdffd58fb55759ebd116a9e005aa
AlexandrVinokurov <aavinokurov@outlook.com>
2021-03-05 11:24:36 +0300

    Creat: GlobalStringVars & PlayerController scripts

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WildBall.Inputs
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerInput : MonoBehaviour
+    {
+        private Rigidbody playerRigidbody;
+
+        private void Awake()
+        {
+            playerRigidbody = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("Press [A]");
+            }
+
+            if(Input.GetButtonDown(GlobalStringVars.JUMP_BUTTON))
+            {
+                Debug.Log("[Jump button] pressed");
+            }
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Fire");
+            }
+
+            //Debug.Log(Input.GetAxis(GlobalStringVars.HORIZONTAL_AXIS));
+            //Debug.Log(Input.GetAxis(GlobalStringVars.VERTICAL_AXIS));
+        }
+    }
+}
